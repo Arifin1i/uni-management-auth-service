c5482079044189b6579d9434544db7ca2fa037e4
Arifin1i <108435895+Arifin1i@users.noreply.github.com>
2023-06-12 12:27:06 +0100

    module 13.2

--- src/config/dist/index.js
+++ src/config/dist/index.js
@@ -1,10 +1,12 @@
-"use strict";
-exports.__esModule = true;
-var dotenv_1 = require("dotenv");
-var path_1 = require("path");
-dotenv_1["default"].config({ path: path_1["default"].join(process.cwd(), '.env') });
-exports["default"] = {
-    port: process.env.PORT,
-    database_url: process.env.DATABASE_URL,
-    default_user_pass: process.env.DEFAULT_USER_PASS
-};
+'use strict'
+exports.__esModule = true
+var dotenv_1 = require('dotenv')
+var path_1 = require('path')
+dotenv_1['default'].config({
+  path: path_1['default'].join(process.cwd(), '.env'),
+})
+exports['default'] = {
+  port: process.env.PORT,
+  database_url: process.env.DATABASE_URL,
+  default_user_pass: process.env.DEFAULT_USER_PASS,
+}
